722f5451ab0c0707b16d32b87d7240a73fe2bcde
lanefaison2 <faisonusmc@gmail.com>
2017-03-31 23:23:19 -0600

    auth0 not working

--- app.js
+++ app.js
@@ -14,88 +14,51 @@ const Auth0Strategy = require('passport-auth0')
 const cors = require('cors')
 const app = express()
 
-var routes = require('./routes/index');
-var user = require('./routes/user');
-
-// This will configure Passport to use Auth0
-var strategy = new Auth0Strategy({
-    domain:       process.env.AUTH0_DOMAIN,
-    clientID:     process.env.AUTH0_CLIENT_ID,
-    clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    callbackURL:  process.env.AUTH0_CALLBACK_URL || 'http://localhost:8000/callback'
-  }, function(accessToken, refreshToken, extraParams, profile, done) {
-    // accessToken is the token to call Auth0 API (not needed in the most cases)
-    // extraParams.id_token has the JSON Web Token
-    // profile has all the information from the user
-    return done(null, profile);
-  });
-
-passport.use(strategy);
-
-// you can use this section to keep a smaller payload
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(user, done) {
-  done(null, user);
-});
-
 // view engine setup
-// app.set('view engine', 'jade');
-// app.set('views', './views'));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.set('view engine', 'jade')
+// app.set('views', './views')
+app.use(express.static(path.join(__dirname, 'public')))
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(cors({
   origin: ['http://localhost:8000'],
   credentials: true
 }))
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
 app.use(session({
   secret: 'shhhhhhhhh',
   resave: false,
   saveUninitialized: true
-}));
-app.use(passport.initialize());
-app.use(passport.session());
+}))
 
-app.use('/', index);
-app.use('/user', user);
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use('/', index)
+app.use('/user', user)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 // error handlers
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  res.send(err.message);
 });
 
-module.exports = app;
+module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,37 @@
-var express = require('express');
-var passport = require('passport');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+require('dotenv').config()
+var passport = require('passport')
 
-var env = {
-  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
-  AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
-  AUTH0_CALLBACK_URL: 'http://localhost:8000/callback'
-};
+// var env = {
+//   AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+//   AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+//   AUTH0_CALLBACK_URL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:8000/callback'
+// }
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.status(200).send('hello')
-  // res.render('index', { title: 'Express', env: env });
-});
+  // res.render('index', { title: 'Express', env: env })
+})
 
 router.get('/login',
   function(req, res){
-    res.render('login', { env: env });
-  });
+    res.render('login', { env: env })
+})
 
 router.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/');
-});
+  req.logout()
+  res.redirect('/')
+})
 
 router.get('/callback',
   passport.authenticate('auth0', { failureRedirect: '/url-if-something-fails' }),
   function(req, res) {
     console.log(req.user)
     res.redirect('http://localhost:8000/user.html')
-    // res.redirect(req.session.returnTo || 'http://localhost:8000/user.html');
+    // res.redirect(req.session.returnTo || 'http://localhost:8000/user.html')
 
-  });
+})
 
-
-module.exports = router;
+module.exports = router
